86503d06c9c7ec815038b7aab5db9731b6ac36ca
Josep <josep.alos.pascual@hotmail.com>
2019-11-05 16:30:46 +0100

    Added a basic class for storing pokemons

--- PokemonWebSpider/scrapper.py
+++ PokemonWebSpider/scrapper.py
@@ -2,6 +2,7 @@
 from dataclasses import dataclass
 from typing import List
 
+
 @dataclass
 class Pokemon:
     number: int
@@ -13,13 +14,34 @@ class Pokemon:
     evolves_from: int
     evolves_to: List[int]
 
+    def __repr__(self):
+        return "Pokemon [{}, {}]".format(self.number, self.name)
+
+
+class PokemonStorage:
+    _pokemons = dict()
+    @classmethod
+    def store_pokemon(cls, pokemon: Pokemon):
+        cls._pokemons[pokemon.number] = pokemon
+
+    @classmethod
+    def store_pokemons(cls, pokemons: List[Pokemon]):
+        for pokemon in pokemons:
+            cls.store_pokemon(pokemon)
+
+    @classmethod
+    def get_pokemon(cls, identifier: int) -> Pokemon:
+        return cls._pokemons[identifier]
+
+
 def main():
-    bulbasaur = Pokemon(1,
-                        "Bulbasaur",
-                        "grass",
-                        "poison",
-                        None,
-                        2)
+    PokemonStorage.store_pokemons([
+        Pokemon(1, "Bulbasaur", "grass", "poison", None, 2),
+        Pokemon(2, "Ivysaur", "grass", "poison", 1, 3),
+        Pokemon(3, "Venusaur", "grass", "poison", 2, 3),
+    ])
+
+    print(PokemonStorage.get_pokemon(2))
 
 if __name__ == "__main__":
     main()
