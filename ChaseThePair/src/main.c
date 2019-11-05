2d050dd6c133b35949b64188d774dcd6204bf54b
Josep <josep.alos.pascual@hotmail.com>
2019-11-05 10:39:12 +0100

    Refactored code

--- ChaseThePair/src/main.c
+++ ChaseThePair/src/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <ctype.h>
 #include <limits.h>
 
 #define MAX_ELEMENTS_TO_PRINT 10
@@ -20,8 +21,8 @@ void print_array(Array arr);
 int chase_value(Array array, int value);
 
 int main(int argc, char* argv[]){
-	clock_t start, end;
-	double load_cpu_time, chase_cpu_time;
+	clock_t start, end, start_v, end_v;
+	double load_cpu_time, chase_cpu_time, chase_v1_cpu_time, chase_v2_cpu_time;
 	Array a;
 	Array b;
 	int chase;
@@ -50,20 +51,32 @@ int main(int argc, char* argv[]){
 	printf("Start chasing value %d...\n", chase);
 	start = clock();
 
+	start_v = clock();
 	v1 = chase_value(a, chase);
+	end_v = clock();
+	chase_v1_cpu_time = (double) (end_v - start_v);
 	free(a.values);
 
+	start_v = clock();
 	v2 = chase_value(b, chase);
+	end_v = clock();
+	chase_v2_cpu_time = (double) (end_v - start_v);
 	free(b.values);
 
 	end = clock();
+
+	chase_v1_cpu_time /= CLOCKS_PER_SEC;
+	chase_v2_cpu_time /= CLOCKS_PER_SEC;
 	chase_cpu_time = ((double) (end - start)) / CLOCKS_PER_SEC;
 
-	printf("\n");
-	printf("Loading time: %f\n", load_cpu_time);
-	printf("Chasing time: %f\n", chase_cpu_time);
-	printf("Total time: %f\n", load_cpu_time + chase_cpu_time);
+
 	printf("Pair is (%d, %d)\n", v1, v2);
+	printf("\nSTATISTICS\n");
+	printf("Loading time:\t\t%f\n", load_cpu_time);
+	printf("Chasing vector 1:\t%f\n", chase_v1_cpu_time);
+	printf("Chasing vector 2:\t%f\n", chase_v2_cpu_time);
+	printf("Total Chasing time:\t%f\n", chase_cpu_time);
+	printf("Total time:\t\t%f\n", load_cpu_time + chase_cpu_time);
 
 }
 
@@ -93,8 +106,34 @@ void push(Array *arr, int value){
 	arr->length += 1;
 }
 
-void parse_set(FILE *fp, Array * arr){
+int get_next_int(FILE *fp, int * next_digit){
+	int current_value;
+	int found = -1;
 	char current_c;
+
+	current_value = 0;
+	do{
+		current_c = (char) fgetc(fp);
+		if(!isdigit(current_c)){
+			break;
+		}
+		found = 0;
+		current_value = current_value * 10 + (current_c - '0');
+	}while(1);
+	*next_digit = current_value;
+
+	return found;
+}
+
+void seek_to_vector_start(FILE *fp){
+	char current_c;
+	// Read until vector starts
+	do{
+		current_c = (char) fgetc(fp);
+	}while(current_c != EOF && current_c != '[');
+}
+
+void parse_set(FILE *fp, Array * arr){
 	int current_value;
 
 	arr->values = (int*) malloc(INITIAL_CAPACITY * sizeof(int));
@@ -102,25 +141,12 @@ void parse_set(FILE *fp, Array * arr){
 	arr->capacity = INITIAL_CAPACITY;
 
 	// Read until vector starts
-	do{
-		current_c = (char) fgetc(fp);
-	}while(current_c != EOF && current_c != '[');
+	seek_to_vector_start(fp);
 
 	// Read integers from vector
 	current_value = 0;
-	while(current_c != EOF){
-		current_c = (char) fgetc(fp);
-		
-		if (current_c == ','){
-			push(arr, current_value);
-			current_value = 0;
-		} else if (current_c == EOF || current_c == ']'){
-			push(arr, current_value);
-			current_value = 0;
-			break;
-		} else {
-			current_value = current_value * 10 + (current_c - '0');
-		}
+	while(get_next_int(fp, &current_value) != -1){
+		push(arr, current_value);
 	}
 }
 
