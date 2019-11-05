f6294eb30973929b2216208c81ed58ec7a3b8f98
Josep <josep.alos.pascual@hotmail.com>
2019-11-05 16:31:43 +0100

    Extract the pokemons classes in a new file

--- PokemonWebSpider/scrapper.py
+++ PokemonWebSpider/scrapper.py
@@ -1,38 +1,5 @@
 #/usr/bin/env python3.7
-from dataclasses import dataclass
-from typing import List
-
-
-@dataclass
-class Pokemon:
-    number: int
-    name: str
-    type1: str
-    type2: str
-    # sprite: TODO
-    # Evolution chain is represented by identifiers, not the pokemon object itself.
-    evolves_from: int
-    evolves_to: List[int]
-
-    def __repr__(self):
-        return "Pokemon [{}, {}]".format(self.number, self.name)
-
-
-class PokemonStorage:
-    _pokemons = dict()
-    @classmethod
-    def store_pokemon(cls, pokemon: Pokemon):
-        cls._pokemons[pokemon.number] = pokemon
-
-    @classmethod
-    def store_pokemons(cls, pokemons: List[Pokemon]):
-        for pokemon in pokemons:
-            cls.store_pokemon(pokemon)
-
-    @classmethod
-    def get_pokemon(cls, identifier: int) -> Pokemon:
-        return cls._pokemons[identifier]
-
+from pokemons import Pokemon, PokemonStorage
 
 def main():
     PokemonStorage.store_pokemons([
